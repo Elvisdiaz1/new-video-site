890770e7b7b2d500dc72251c8a7ebe8f8c6db11f
Elvis Diaz <elvisdiaz020@gmail.com>
2023-12-17 15:20:45 -0500

    creates navbar with styling

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import user from "./images/dummy-user-pic.jpg";
 
 const Navbar = () => {
   return (
     <div>
       <nav id="navbar">
         <h3>Logo</h3>
-        <h4>Welcome</h4>
+        <h3>Manic</h3>
+
+        {/* Insert search bar */}
         {/* Person Little Profile on Navbar */}
         <h5>Jan Dae</h5>
-        <img src="./person-2 copy.jpg" alt="Jan" />
+        <img src={user} alt="account user" />
       </nav>
     </div>
   );
