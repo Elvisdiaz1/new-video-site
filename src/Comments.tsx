0e28bb36f19f4a60420f2b63a4e8fa3874b14f13
Elvis Diaz <elvisdiaz020@gmail.com>
2024-04-11 14:28:08 -0400

    adds comment section with placeholders and styling

---
+++ src/Comments.tsx
@@ -0,0 +1,16 @@
+const Comments = () => {
+  return (
+    <div id="comment-container">
+      <div id="comments">
+        <h2>Comments</h2>
+        <ul>
+          <li>Commenter 1: Letsssss Gooooooo</li>
+          <li>Commenter 2: I loved this game when I was younger.</li>
+          <li>Commenter 3: Did you know you can jump LOL</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
--- src/Main_Page.tsx
+++ src/Main_Page.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Menu from "./Menu";
 import Side_Videos from "./Side_Videos";
+import Comments from "./Comments";
 // import video from "../public/videos/test.mp4";
 
 const Main_Page = () => {
   return (
     <div id="main-container">
       <Menu />
-      <div>
+      <div id="video-body">
         <h3>hel</h3>
         <video controls autoPlay={true} muted id="video">
           <source src="videos/test.mp4" type="video/mp4" />
         </video>
+        <Comments />
       </div>
 
       <Side_Videos />
