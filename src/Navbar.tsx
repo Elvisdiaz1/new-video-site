64278c2132b8610892ed86723f79888d226eaacc
Elvis Diaz <elvisdiaz020@gmail.com>
2024-04-15 14:10:53 -0400

    adds div containers for the different sections in the navbar, made them flex, and added gap spacing in logo and title and added padding to search bar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -5,13 +5,20 @@ const Navbar = () => {
   return (
     <div id="nav-container">
       <nav id="navbar">
-        <h3>Logo</h3>
-        <h3>Manic</h3>
-        <input type="text" placeholder="Search" />
-        <button> Submit</button>
-        {/* Person Little Profile on Navbar */}
-        <h5>Jan Dae</h5>
-        <img src={user} alt="account user" />
+        <div id="website-naming">
+          <h3 id="website-logo">Logo</h3>
+          <h3 id="website-title">Manic</h3>
+        </div>
+
+        <div id="search-bar">
+          <input type="text" placeholder="Search" />
+          <button> Submit</button>
+        </div>
+        <div id="account">
+          {/* Person Little Profile on Navbar */}
+          <h5>Jan Dae</h5>
+          <img src={user} alt="account user" />
+        </div>
       </nav>
     </div>
   );
